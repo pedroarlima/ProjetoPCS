765ac9e63fc10249d6c0ce1f1202063e10cacdd9
Pedro Lima <pedroarlima@gmail.com>
2015-11-02 17:22:55 -0200

    Create PERSONAGEM.cpp

---
+++ PERSONAGEM.cpp
@@ -0,0 +1,68 @@
+#include "PERSONAGEM.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+personagem::personagem()
+{
+  HitPontos = ;
+}
+
+void personagem::setHP()
+{
+  
+
+}
+
+int personagem::getHP()
+{
+  return HitPontos;
+}
+
+void personagem::ColisaoFront()
+{
+
+}
+
+void personagem::ColisaoBot()
+{
+
+}
+
+void personagem::andar()
+{
+
+}
+
+void personagem::pular()
+{
+
+}
+
+void personagem::ComerBem()
+{
+
+}
+
+void personagem::ComerMal()
+{
+
+}
+
+void personagem::AumentaForca()
+{
+
+}
+
+void personagem::DiminuiuForca()
+{
+
+}
+
+void personagem::VerificaAlimento()
+{
+
+}
+    
+};
